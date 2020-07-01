a9eae381751550793becaa886df916bdec8b42be
anton <antonsyski@gmail.com>
2020-07-01 13:52:43 +0300

    upd

--- README.md
+++ README.md
@@ -19,6 +19,7 @@ You’ll start by editing this README file to learn how to edit a file in Bitbuc
 
 ---
 
+
 ## Create a file
 
 Next, you’ll add a new file to this repository.
--- engine/src/shader.cxx
+++ engine/src/shader.cxx
@@ -100,7 +100,7 @@ shader::shader(const char* vert_shader_path, const char* frag_shader_path)
                   << info_chars.data();
     }
 
-    // gfx programm
+    // gfx   programm
 
     id = glCreateProgram();
     OM_GL_CHECK()
