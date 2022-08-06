3c3bc99a0ae0c8008c22eeef2f06bc71bdf4f20f
Anton <antonsyski@gmail.com>
2022-08-06 12:38:09 +0300

    added default construction for vbo

--- engine/vbo.cxx
+++ engine/vbo.cxx
@@ -464,6 +464,63 @@ vbo_8::~vbo_8()
     GL_CHECK()
 }
 
+vbo_8::vbo_8()
+    : vbo_data{
+        { -0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f },
+        { -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f },
+        { 0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f },
+        { 0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f },
+    },
+    //TODO: make shure that the ebo are interpritate ctrangles are correctly
+    //            1
+    //           0.5
+    // -1  -0.5   0   0.5   1
+    //          -0.5
+    //           -1
+
+    ebo_data{0,1,2,2,1,3}
+{
+    // add vertex buffer and indexes buffer
+    // add buffers uploading and deletting
+    // add vertexa object bufer and explain buffer layout for opengl
+
+    glGenVertexArrays(1, &vao_id);
+    glGenBuffers(1, &vbo_id);
+
+    glGenBuffers(1, &ebo_id);
+    GL_CHECK();
+    bind_vao();
+    glBindBuffer(GL_ARRAY_BUFFER, vbo_id);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(v_8) * vbo_data.size(),
+                 vbo_data.data(), GL_STATIC_DRAW);
+    GL_CHECK();
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo_id);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(uint32_t) * index_count,
+                 ebo_data.data(), GL_DYNAMIC_DRAW);
+    GL_CHECK();
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(v_8), nullptr);
+    GL_CHECK();
+    glEnableVertexAttribArray(0);
+    GL_CHECK();
+
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(v_8),
+                          (void*)(sizeof(v_3)));
+    GL_CHECK();
+    glEnableVertexAttribArray(1);
+    GL_CHECK();
+
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(v_8),
+                          (void*)(sizeof(v_3) * 2));
+    GL_CHECK();
+    glEnableVertexAttribArray(2);
+    GL_CHECK();
+
+    glBindVertexArray(0);
+    GL_CHECK();
+}
+
 vbo_8::vbo_8(std::string path)
 {
     std::fstream vertex_file;
